9e61b730ca631fd80e6dfca05dcbc2f9b7c1f3f9
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-09-13 12:37:23 +0800

    MOD

--- project-auction-javascript/BE/routes/v1/index.js
+++ project-auction-javascript/BE/routes/v1/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const rolesRoute = require("../v1/roles.routes");
+const rolesRoute = require("./roles.routes");
 
 router.use("/roles", rolesRoute);
 module.exports = router;
